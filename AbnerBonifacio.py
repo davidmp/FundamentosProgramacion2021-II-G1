bc1f9e784d1026963dc8f8570e3a1fc2c13ae237
AbnerBonifacio <abnerbonifacio@upeu.edu.pe>
2021-09-13 15:50:59 +0000

    clave c

--- AbnerBonifacio.py
+++ AbnerBonifacio.py
@@ -1,7 +1,8 @@
 print("Hola Mundo....Como estan!!")
 a=50
 b=5
+c=2
 print("Suma:", (a+b))
 print("Resta:", (a-b))
 print("Multiplicacion:", (a*b))
-print("Division:", (a/b))
+print("Division:", (a/c)
