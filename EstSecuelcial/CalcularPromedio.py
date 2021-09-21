c4250ea5ef46668584102826ab3033169730d001
davidmamani <>
2021-09-21 13:58:20 +0000

    calcular promedio final

---
+++ EstSecuelcial/CalcularPromedio.py
@@ -0,0 +1,14 @@
+print("Calcular Promedio final de FP")
+#Datos de Entrada
+nota1=int(input("ingrese nota de evaluacion 1:"))
+nota2=int(input("ingrese nota de evaluacion 2:"))
+nota3=int(input("ingrese nota de evaluacion 3:"))
+nota4=int(input("ingrese nota de evaluacion 4:"))
+nota5=int(input("ingrese nota de evaluacion 5:"))
+nota6=int(input("ingrese nota de evaluacion 6:"))
+nota7=int(input("ingrese nota de evaluacion 7:"))
+#Prceso
+proFin=(0.15*nota1)+(0.15*nota2)+(0.10*nota3)+(0.15*nota4)+(0.05*nota5)+(0.10*nota6)+(0.30*nota7)
+#Datos de Salida
+print(type(proFin))
+print("El promedio Final es:", round(proFin,2))
