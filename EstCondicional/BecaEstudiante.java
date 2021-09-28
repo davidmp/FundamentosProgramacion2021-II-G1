03eeebce06025afe67734ba7b87203cf967df202
davidmamani <>
2021-09-28 15:15:16 +0000

    Ejemplo est. Cond. Multiple

---
+++ EstCondicional/BecaEstudiante.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+public class BecaEstudiante {
+ static Scanner sc = new Scanner(System.in);
+
+static void becaPresEst(){
+    //Declaracion de variables e inicializacion
+    int edad=0;
+    double promedio=0, montoBeca=0;
+    //Datos de Entrada
+    System.out.println("Ingrese su edad:");
+    edad=sc.nextInt();
+    System.out.println("Ingrese su promedio de nota:");
+    promedio=sc.nextDouble();    
+    //Proceso
+    if(edad>18){
+      if(promedio>=9){
+        montoBeca=2000;
+      }else if(promedio>=7.5 && promedio<9){
+        montoBeca=1000;
+      }else if(promedio>=6 && promedio<7.5){
+        montoBeca=500;
+      }else{
+        montoBeca=0;
+      }
+    }else{
+      if(promedio>=9){
+        montoBeca=3000;
+      }else if(promedio>=8 && promedio<9){
+        montoBeca=2000;
+      }else if(promedio<8 && promedio>=6){
+        montoBeca=100;
+      }else{
+        montoBeca=0;
+      }
+    }
+    //Datos Salida
+    if(montoBeca==0){
+      System.out.println("Esforzarse para el proximo ciclo...!");
+    }else{
+      System.out.println("Felicitaciones Ud. es acreedor de una Beca por el Monto de: $"+montoBeca);
+    }
+}
+
+
+static void mostrarDiaLetras(){
+  //Definir variables
+  int dia=0;
+  String nombredia="";
+  //Datos de Entrada
+  System.out.println("Ingrese un dia de la semana entre (1-7):");
+  dia=sc.nextInt();
+  //Proceso
+  switch(dia) {
+    case 1: nombredia="Domingo";break;
+    case 2: nombredia="Lunes";break;
+    case 3: nombredia="Martes";break;
+    case 4: nombredia="Miercoles";break;
+    case 5: nombredia="Jueves";break;
+    case 6: nombredia="Viernes";break;
+    case 7: nombredia="Sabado";break;
+    default:
+      nombredia="No existe";
+  }
+  //Datos de Salida
+  System.out.println("El dia es: "+nombredia);
+}
+
+
+
+
+
+public static void main(String[] args) {
+  //becaPresEst();
+  mostrarDiaLetras();
+}
+
+}
--- EstCondicional/PagoServTelef.java
+++ EstCondicional/PagoServTelef.java
@@ -12,7 +12,48 @@ public class PagoServTelef {
     System.out.println("Nombre ingresado es:"+nombre+" y la edad es:"+edad);
   }
 
+  static void pagoservicioTelef(){
+    //Definir variables
+    String dia="", turno="";
+    int minL=0;
+    double pagoL=0, pImpuesto=0, totalPago=0;
+    //Datos de entrada
+    System.out.print("Ingrese Min de llamada:");
+    minL=sc.nextInt();
+    System.out.print("\nDia de llamada:");
+    dia=sc.next().toUpperCase();
+    System.out.print("\nTurno de llamada:");
+    turno=sc.next().toUpperCase();    
+    //Proceso
+    if(minL<=5){
+      pagoL=minL*1;
+    }else if(minL>5 && minL<=8){
+      pagoL=(5*1)+((minL-5)*0.8);
+    }else if(minL>8 && minL<=10){
+      pagoL=(5*1)+(3*0.8)+((minL-8)*0.7);
+    }else{
+      pagoL=(5*1)+(3*0.8)+(2*0.7)+((minL-10)*0.5);
+    }
+    if(dia.equals("D")|| dia.equals("S")){ //dias no habiles
+        if(dia.equals("D")){
+          pImpuesto=pagoL*0.03;
+        }
+    }else{//Dias habiles
+      if(turno.equals("M")){
+        pImpuesto=pagoL*0.15;
+      }else{
+        pImpuesto=pagoL*0.10;
+      }
+    }
+    totalPago=pagoL+pImpuesto;
+    //Datos de salida
+    System.out.println("Pago por llamada:"+pagoL);
+    System.out.println("Impuesto:"+pImpuesto);
+    System.out.println("Total Pago:"+totalPago);
+  }
+
+
   public static void main(String[] args) {
-  practica();
+    pagoservicioTelef();
   }
 }
