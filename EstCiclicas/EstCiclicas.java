2fb5d2fac2cf35490d2a5e979783165fec91d355
davidmp <mamanipari@gmail.com>
2021-10-12 13:46:39 +0000

    Factorial

--- EstCiclicas/EstCiclicas.java
+++ EstCiclicas/EstCiclicas.java
@@ -48,48 +48,63 @@ static void suma20mumerosParesFor(){
 }
 
 
-  static void asignaturasCursadas(){
-    //definir variables e inicializacion de valores
-    int asigCod, calf, cred, cantAsigCur=0, cont=0;
-    double promPond=0, sumCred=0, temp=0;
-    //Datos de entrada proceso
-    System.out.println("Ingrese el codigo la asignatura:");
+static void asignaturasCursadas(){
+//definir variables e inicializacion de valores
+int asigCod, calf, cred, cantAsigCur=0, cont=0;
+double promPond=0, sumCred=0, temp=0;
+//Datos de entrada proceso
+System.out.println("Ingrese el codigo la asignatura:");
+asigCod=sc.nextInt();
+while(asigCod!=9999){
+    System.out.println("Ingrese la calificacion del curso "+asigCod+":");
+    calf=sc.nextInt();
+    System.out.println("Ingrese el numero de creditos del curso "+asigCod+":");
+    cred=sc.nextInt();
+    temp=temp+(calf*cred);
+    sumCred=sumCred+cred;
+    cont++;
+    System.out.println("Ingrese el codigo la siguiente asignatura:");
     asigCod=sc.nextInt();
-    while(asigCod!=9999){
-      System.out.println("Ingrese la calificacion del curso "+asigCod+":");
-      calf=sc.nextInt();
-      System.out.println("Ingrese el numero de creditos del curso "+asigCod+":");
-      cred=sc.nextInt();
-      temp=temp+(calf*cred);
-      sumCred=sumCred+cred;
-      cont++;
-      System.out.println("Ingrese el codigo la siguiente asignatura:");
-      asigCod=sc.nextInt();
-        if(asigCod==9999){
-          if(sumCred>=25 && sumCred<=50){
-            promPond=Math.round((temp/sumCred)*100.0)/100.0;
-          }else{
-            System.out.println("La cantidad de creditos no estan entre los rangos permitidos..intente nuevamente");
-            System.out.println("Ingrese el codigo la asignatura:");
-            asigCod=sc.nextInt();
-            temp=0;
-            sumCred=0;
-            cont=0;
-          }
+    if(asigCod==9999){
+        if(sumCred>=25 && sumCred<=50){
+        promPond=Math.round((temp/sumCred)*100.0)/100.0;
+        }else{
+        System.out.println("La cantidad de creditos no estan entre los rangos permitidos..intente nuevamente");
+        System.out.println("Ingrese el codigo la asignatura:");
+        asigCod=sc.nextInt();
+        temp=0;
+        sumCred=0;
+        cont=0;
         }
     }
-    //dados de salida
-    System.out.println("Cantidad de Asignaturas cursadas es:"+cont);
-    System.out.println("Suma de creditos cursados:"+sumCred);
-    System.out.println("Promedio Ponderado:"+promPond);
-  }
+}
+//dados de salida
+System.out.println("Cantidad de Asignaturas cursadas es:"+cont);
+System.out.println("Suma de creditos cursados:"+sumCred);
+System.out.println("Promedio Ponderado:"+promPond);
+}
 
+static int factorial(int n){
+    //declarar variables
+    int resultF=1;
+    //Proceso
+    if(n>1){
+        for(int i=1; i<=n;i++){
+            resultF=resultF*i;
+        }
+    }
+    //Datos de salida
+    return resultF;
+}
 
 
   static void menuOpciones(){ 
     int opcionesA=0;
     System.out.println("-------------------------------");
-    String msg="\nIngrese el Algoritmo que desea Probar 1=xxxx,2=xxxx,3=numeros pares\n4=Cantdad Cursos y detalle";
+    String msg="\nIngrese el Algoritmo que desea Probar"+ 
+    "1=xxxx,2=xxxx,3=numeros pares"+
+    "\n4=Cantdad Cursos y detalle"+
+    "\n5=Factorial";
     System.out.println(msg);
     opcionesA=sc.nextInt();  
     while(opcionesA!=0){
@@ -98,10 +113,14 @@ static void suma20mumerosParesFor(){
           case 2: suma20mumerosParesFor();break;
           case 3: suma20numerosParesDowhile(); break;
           case 4: asignaturasCursadas(); break;
+          case 5: 
+          System.out.println("Ingrese un numero para calcular el factorial:");
+          int n=sc.nextInt();
+          System.out.println("El Factorial de "+n+" es:"+factorial(n)); break;
           default:
             System.out.println("El Algoritmo no existe!");
         }   
-      System.out.println("------------------------------");  
+      System.out.println("------------------------------"); //3.628.800 
       System.out.println(msg);
       opcionesA=sc.nextInt();        
     }
