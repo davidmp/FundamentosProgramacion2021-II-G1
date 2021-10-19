5d84261d6a74ef0eb607d9315c8289d9a1e30d7f
davidmamani <>
2021-10-19 13:38:18 +0000

    Proyecto de tipo Maven

--- EstCiclicas/EstCiclicas.java
+++ EstCiclicas/EstCiclicas.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 class EstCiclicas{
   static Scanner sc = new Scanner(System.in);
+  
 
 static void suma20mumerosParesWhile(){
   //Declarar variables e inicializacion de valores
@@ -48,41 +49,40 @@ static void suma20mumerosParesFor(){
     System.out.println("La suma de numeros pares entre 1 y 20 es:"+sumaNumPar);
 }
 
-
 static void asignaturasCursadas(){
-//definir variables e inicializacion de valores
-int asigCod, calf, cred, cantAsigCur=0, cont=0;
-double promPond=0, sumCred=0, temp=0;
-//Datos de entrada proceso
-System.out.println("Ingrese el codigo la asignatura:");
-asigCod=sc.nextInt();
-while(asigCod!=9999){
-    System.out.println("Ingrese la calificacion del curso "+asigCod+":");
-    calf=sc.nextInt();
-    System.out.println("Ingrese el numero de creditos del curso "+asigCod+":");
-    cred=sc.nextInt();
-    temp=temp+(calf*cred);
-    sumCred=sumCred+cred;
-    cont++;
-    System.out.println("Ingrese el codigo la siguiente asignatura:");
-    asigCod=sc.nextInt();
-    if(asigCod==9999){
-        if(sumCred>=25 && sumCred<=50){
-        promPond=Math.round((temp/sumCred)*100.0)/100.0;
-        }else{
-        System.out.println("La cantidad de creditos no estan entre los rangos permitidos..intente nuevamente");
-        System.out.println("Ingrese el codigo la asignatura:");
-        asigCod=sc.nextInt();
-        temp=0;
-        sumCred=0;
-        cont=0;
-        }
-    }
-}
-//dados de salida
-System.out.println("Cantidad de Asignaturas cursadas es:"+cont);
-System.out.println("Suma de creditos cursados:"+sumCred);
-System.out.println("Promedio Ponderado:"+promPond);
+  //definir variables e inicializacion de valores
+  int asigCod, calf, cred, cantAsigCur=0, cont=0;
+  double promPond=0, sumCred=0, temp=0;
+  //Datos de entrada proceso
+  System.out.println("Ingrese el codigo la asignatura:");
+  asigCod=sc.nextInt();
+  while(asigCod!=9999){
+      System.out.println("Ingrese la calificacion del curso "+asigCod+":");
+      calf=sc.nextInt();
+      System.out.println("Ingrese el numero de creditos del curso "+asigCod+":");
+      cred=sc.nextInt();
+      temp=temp+(calf*cred);
+      sumCred=sumCred+cred;
+      cont++;
+      System.out.println("Ingrese el codigo la siguiente asignatura:");
+      asigCod=sc.nextInt();
+      if(asigCod==9999){
+          if(sumCred>=25 && sumCred<=50){
+          promPond=Math.round((temp/sumCred)*100.0)/100.0;
+          }else{
+          System.out.println("La cantidad de creditos no estan entre los rangos permitidos..intente nuevamente");
+          System.out.println("Ingrese el codigo la asignatura:");
+          asigCod=sc.nextInt();
+          temp=0;
+          sumCred=0;
+          cont=0;
+          }
+      }
+  }
+  //dados de salida
+  System.out.println("Cantidad de Asignaturas cursadas es:"+cont);
+  System.out.println("Suma de creditos cursados:"+sumCred);
+  System.out.println("Promedio Ponderado:"+promPond);
 }
 
 static long factorial(int n){
---
+++ SubProgramUPeU/README.md
@@ -0,0 +1,6 @@
+# Comandos Proyecto MVN
+  mvn clean
+
+  mvn clean package
+
+  java -jar target/fileProyect.jar
