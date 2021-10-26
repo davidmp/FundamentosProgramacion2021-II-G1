5728504bd09995319e86425264aa18d48f240a4f
davidmamani <>
2021-10-26 13:47:46 +0000

    ejemplo no recursivo Fibonaci

--- SubProgramUPeU/src/main/java/pe/edu/upeu/app/App.java
+++ SubProgramUPeU/src/main/java/pe/edu/upeu/app/App.java
@@ -1,18 +1,52 @@
 package pe.edu.upeu.app;
 import pe.edu.upeu.modulo.*;
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App{
     
    public static SubProgramas sp;
+   public static Recursividad rc=new Recursividad();
+   public static Scanner sc=new Scanner(System.in);
+
+    static void menuOpciones(){ 
+      sp=new SubProgramas();
+      int opcionesA=0;
+      System.out.println("-------------------------------");
+      String msg="\nIngrese el Algoritmo que desea Probar"+ 
+      "\n1=Sualudo"+
+      "\n2=otro saludo"+
+      "\n3=Factorial"+
+      "\n4=Fibonacci"+
+      "\n5=Fibonacci No Recursivo"
+      ;
+      System.out.println(msg);
+      opcionesA=sc.nextInt();  
+      while(opcionesA!=0){
+          switch(opcionesA) {
+            case 1: sp.saludo(); break;
+            case 2: sp.otroSaludo("Raul"); break;
+            case 3: 
+            System.out.println("Ingrese un numero:");
+            long n=sc.nextLong();
+            System.out.println(rc.factorial(n)); break;
+            case 4:   
+            System.out.println("Ingrese un numero:");         
+            int nx=sc.nextInt();
+            System.out.println(rc.fibonacci(nx)); break;
+            case 5: 
+            System.out.println("Ingrese un numero:");       
+            int nxx=sc.nextInt();
+            System.out.println(rc.fibonacciNRecurBig(nxx)); break;            
+            default: System.out.println("El Algoritmo no existe!");
+          }   
+        System.out.println("------------------------------"); //15.511.210.043.330.985.984.000.000
+        System.out.println(msg);
+        opcionesA=sc.nextInt();        
+      }
+    }
 
     public static void main( String[] args ){
-        System.out.println( "Hello World!" );
-         sp=new SubProgramas();
-         sp.saludo();
-         sp.otroSaludo("Raul");
-
+       System.out.println("*******Bienvenidos al Sistema**********");
+       menuOpciones();
     }
 }
---
+++ SubProgramUPeU/src/main/java/pe/edu/upeu/modulo/Recursividad.java
@@ -0,0 +1,55 @@
+package pe.edu.upeu.modulo;
+import java.math.BigInteger;
+
+public class Recursividad{
+  
+
+  public long factorial(int numero){
+    if(numero>1){
+      return factorial(numero-1)*numero;
+    }
+    return 1;
+  }
+
+  public BigInteger factorial(long numero){
+    if(numero>1){
+      return factorial(numero-1).multiply(new BigInteger(String.valueOf(numero)));
+    }
+    return new BigInteger(String.valueOf(1));
+  }
+
+  public long fibonacci(int n){
+    if(n>=2){
+      return fibonacci(n-1)+fibonacci(n-2);
+    }else{
+      return n;
+    }
+  }
+  //0,1,1,2,3,5   ,8,13,21,34
+  public long fibonacciNRecur(int numero){
+    int valAnt=0, valNew=1, numAux=0;    
+    if(numero>1){
+      for(int con=1;con<numero;con++){
+        numAux=valNew;
+        valNew=valAnt+valNew;
+        valAnt=numAux;
+      }
+      return valNew;
+    }
+    return numero;
+  }
+
+  public BigInteger fibonacciNRecurBig(int numero){
+    BigInteger valAnt=new BigInteger("0"), valNew=new BigInteger("1"), numAux=new BigInteger("0");    
+    if(numero>1){
+      for(int con=1;con<numero;con++){
+        numAux=valNew;
+        valNew=valAnt.add(valNew);
+        valAnt=numAux;
+      }
+      return valNew;
+    }
+    return new BigInteger(String.valueOf(numero));
+  }  
+
+}
