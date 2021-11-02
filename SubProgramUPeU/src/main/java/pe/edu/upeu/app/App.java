52ce53a07d27a79f918b3a8c800c2f13a058304c
davidmamani <>
2021-11-02 13:31:51 +0000

    cambiando gitignore y class App

--- SubProgramUPeU/src/main/java/pe/edu/upeu/app/App.java
+++ SubProgramUPeU/src/main/java/pe/edu/upeu/app/App.java
@@ -1,13 +1,11 @@
 package pe.edu.upeu.app;
 import pe.edu.upeu.modulo.*;
 import pe.edu.upeu.util.*;
-import java.util.Scanner;
 
 public class App{
     
    public static SubProgramas sp;
-   public static Recursividad rc=new Recursividad();
-   public static Scanner sc=new Scanner(System.in);
+   public static Recursividad rc=new Recursividad();   
    static LeerTeclado lt=new LeerTeclado();
 
     static void menuOpciones(){ 
@@ -21,28 +19,25 @@ public class App{
       "\n4=Fibonacci"+
       "\n5=Fibonacci No Recursivo"    
       ;
-      System.out.println(msg);
-      opcionesA=sc.nextInt();  
+   
+      opcionesA=lt.leer(0, msg);  
       while(opcionesA!=0){
           switch(opcionesA) {
             case 1: sp.saludo(); break;
             case 2: sp.otroSaludo("Raul"); break;
-            case 3: 
-            System.out.println("Ingrese un numero:");
-            long n=sc.nextLong();
+            case 3:             
+            long n=lt.leer((long)0, "Ingrese un numero");
             System.out.println(rc.factorial(n)); break;
-            case 4:   
-            System.out.println("Ingrese un numero:");         
-            int nx=sc.nextInt();
+            case 4:                    
+            int nx=lt.leer(0,"Ingrese un numero");
             System.out.println(rc.fibonacci(nx)); break;
             case 5: 
             int nxx=lt.leer(0,"Ingrese un numero");
             System.out.println(rc.fibonacciNRecurBig(nxx)); break;
             default: System.out.println("El Algoritmo no existe!");
           }   
-        System.out.println("------------------------------"); //15.511.210.043.330.985.984.000.000
-        System.out.println(msg);
-        opcionesA=sc.nextInt();        
+        System.out.println("------------------------------"); //15.511.210.043.330.985.984.000.000        
+        opcionesA=lt.leer(0,msg);        
       }
     }
 
